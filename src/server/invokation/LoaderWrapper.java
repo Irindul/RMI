015a5b23ecad9ffe0e1fd76ead5af754d08774fc
Irindul <mathieu.regnard2@gmail.com>
2018-09-12 10:02:50 -0400

    Refactored loaded in separate file

---
+++ src/server/invokation/LoaderWrapper.java
@@ -0,0 +1,50 @@
+package server.invokation;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+public class LoaderWrapper {
+
+  private URL url;
+
+  public LoaderWrapper() {
+    String rootPath = System.getProperty("user.dir")
+        + File.separator
+        + "resources"
+        + File.separator;
+
+    File root = new File(rootPath);
+    try {
+      url = root.toURI().toURL();
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Optional<Class<?>> load(String compiledFilePath) {
+    String compiledFileName = getTrimmedCompiledName(compiledFilePath);
+    try {
+      URLClassLoader classLoader = URLClassLoader
+          .newInstance(new URL[]{url}, getClass().getClassLoader());
+
+      //Here we assume package is client
+      String packageStructure = "client.";
+      Class<?> cls = Class.forName(packageStructure + compiledFileName, true, classLoader);
+      return Optional.of(cls);
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+      return Optional.empty();
+    }
+  }
+
+  private String getTrimmedCompiledName(String compiledFilePath) {
+    Path path = Paths.get(compiledFilePath);
+    String compiledFileName = path.getFileName().toString();
+    return compiledFileName.split("\\.")[0];
+  }
+}
--- src/server/states/SourceColl.java
+++ src/server/states/SourceColl.java
@@ -10,16 +10,19 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 import server.invokation.CompilerWrapper;
+import server.invokation.LoaderWrapper;
 import server.network.FileReceiver;
 
 public class SourceColl implements RMIState {
 
   private FileReceiver fileReceiver;
   private CompilerWrapper compiler;
+  private LoaderWrapper loader;
 
   public SourceColl(InputStream inFromClient, OutputStream outToClient) {
     fileReceiver = new FileReceiver(inFromClient, outToClient);
     compiler = new CompilerWrapper();
+    loader = new LoaderWrapper();
   }
 
   @Override
@@ -29,24 +32,10 @@ public class SourceColl implements RMIState {
     optionalFilename.ifPresent(fileName -> {
       Optional<String> optionalCompiledName = compiler.compile(fileName);
       optionalCompiledName.ifPresent(compiledFilePath -> {
-        // Load and instantiate compiled class.
-        String rootPath = System.getProperty("user.dir")
-            + File.separator
-            + "resources"
-            + File.separator;
-        File root = new File(rootPath);
-        Path path = Paths.get(compiledFilePath);
-        String compiledFileName = path.getFileName().toString();
-        compiledFileName = compiledFileName.split("\\.")[0];
-        try {
-          URL url = root.toURI().toURL();
-          URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{url}, getClass().getClassLoader());
-          Class<?> cls = Class.forName("client." + compiledFileName, true, classLoader);
-          Object instance = cls.newInstance();
-          System.out.println(instance);
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | MalformedURLException e) {
-          e.printStackTrace();
-        }
+        Optional<Class<?>> optionalClass = loader.load(compiledFilePath);
+        optionalClass.ifPresent(cls -> {
+          //Execute method;
+        });
       });
     });
   }
